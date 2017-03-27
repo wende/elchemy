4d9360f8e5ea91640e0a14513d04e44a3f0a0503
Krzysztof Wende <chris@neontree.pl>
2017-03-27 21:34:47 +0200

    fixed tuples and function calls

--- src/Compiler.elm
+++ src/Compiler.elm
@@ -4,6 +4,7 @@ import Char
 import Ast
 import Ast.Expression exposing (..)
 import Ast.Statement exposing (..)
+import Tuple exposing (..)
 import List exposing (..)
 
 -- type alias .T = List Int
@@ -51,11 +52,9 @@ elixirT t =
                 "T" :: rest -> (rest |> reverse |> String.join ".") ++ ".t()"
                 whole -> (whole |> reverse |> String.join ".")
 
-
 statement : Statement -> String
 statement s =
     elixirS s 0
-
 
 ind : Int -> String
 ind i = "\n" ++ (List.repeat ((i + 1)*2) " " |> String.join "")
@@ -77,9 +76,9 @@ elixirS s i =
             "defmodule " ++ name ++ " do"
         Comment content ->
             (ind i) ++ "# " ++ content
-        -- That's not a real import. In elixir it's by default
+        -- That's not a real import. In elixir it's called alias
         ImportStatement path Nothing Nothing ->
-            (ind i) ++ "import " ++ String.join "." path
+            (ind i) ++ "alias " ++ String.join "." path
         ImportStatement path Nothing (Just AllExport) ->
             elixirS (ImportStatement path Nothing Nothing) i
         s ->
@@ -88,38 +87,48 @@ elixirS s i =
 elixirE : Expression -> Int -> String
 elixirE e i =
     case e of
-        Application (Variable ["Just"]) arg -> "{:ok, " ++ nestedTuple arg i True ++ "}"
-        Application name args as application -> nestedTuple application i False
-        --  Application (Variable ["Just"]) x -> "{:ok, " ++ nestedTuple x i ++ "}"
-        -- Application (Variable [name]) args ->
-        --     case maybeUpper name of
-        --         Upper name ->
-        --             "{:" ++ name ++ ", " ++  elixirE args i ++ "}"
-        --         Lower name ->
-        --             name  ++ ".(" ++ elixirE args (i + 1) ++ ")"
-        -- Application fun args as app ->
-        --     nestedTuple app i
-        --     --elixirE fun (i + 1) ++ ".(" ++ elixirE args (i + 1) ++ ")"
+        Application (Variable ["Err"]) arg ->
+            "{:error, " ++ nestedTuple arg i ++ "}"
+
+        Application (Variable ["Ok"]) arg ->
+            "{:ok, " ++ nestedTuple arg i ++ "}"
+
+        Application (Variable ["Just"]) arg ->
+            elixirE arg i
+
+        Variable ["Nothing"] ->
+            "nil"
+
+        Variable [name] ->
+            if isCapitilzed name then
+                atomize name
+            else
+                name
+
+        Application name arg as application ->
+            nestedTuple application i
+
         Integer value ->
             toString value
-        -- Maybe.Just a ->
-        --     "{:ok, " ++ a ++ "}"
+
         String value ->
             toString value
-        Variable ["Nothing"] -> "{:error, " ++ nothing ++ "}"
-        Variable [name] ->
-            name
+
         BinOp (Variable ["::"]) a b ->
             "[" ++
             String.join " "
                 (List.map (\a -> elixirE a (i + 1)) [a, Variable ["|"], b] ) ++
             "]"
+
         BinOp op a b ->
             String.join " " (List.map (\a -> elixirE a (i + 1)) [a, op, b] )
+
         Case var body ->
             caseE var body (i + 1)
+
         Lambda args body ->
             lambda args body i
+
         e ->
             "Not implemented yet for expression: " ++ toString e
 
@@ -132,6 +141,7 @@ lambda args body i =
             ++ " end"
         [] ->
             elixirE body i
+
 tree : String -> String
 tree m =
     case Ast.parse m of
@@ -139,10 +149,8 @@ tree m =
             ((List.map statement statements) |> (List.foldr (++) "")
                          |> flip (++) glueEnd
                          |> (++) glueStart)
-
         err ->
             toString err
-
 
 caseE : Expression -> List (Expression, Expression) -> Int -> String
 caseE var body i =
@@ -166,6 +174,22 @@ maybeUpper string =
             else Lower string
         Nothing -> Lower ""
 
+capitalize : String -> String
+capitalize s =
+    String.uncons s
+        |> Maybe.map (\a -> String.cons (Char.toUpper (first a)) (second a))
+        |> Maybe.withDefault ""
+
+atomize : String -> String
+atomize s =
+    ":" ++ String.toLower s
+
+isCapitilzed : String -> Bool
+isCapitilzed s =
+    String.uncons s
+        |> Maybe.map (\a -> a |> first |> Char.isUpper)
+        |> Maybe.withDefault False
+
 isTuple : Expression -> Bool
 isTuple a =
     case a of
@@ -176,26 +200,30 @@ isTuple a =
                 Lower _ -> False
         other -> False
 
-nestedTuple : Expression -> Int -> Bool -> String
-nestedTuple a i alreadyIn =
-    let
-        (prefix, suffix) = if isTuple a && not alreadyIn then ("{", "}") else ("", "")
-    in
-    prefix ++ (case a of
-        Application (Variable [name]) args ->
-            case maybeUpper name of
-                Upper name ->
-                    ":" ++ (String.toLower name) ++ ", " ++  elixirE args i
-                Lower name ->
-                    name  ++ ".(" ++ elixirE args (i + 1) ++ ")"
-        -- Application fun args as app ->
-        --     nestedTuple app i
-        Application (Application name _ as app) args ->
-            (nestedTuple app i True) ++ ", " ++ elixirE args i
-        Application name args ->
-            elixirE name i ++ "," ++ nestedTuple args i True
-        other -> elixirE (Debug.log "Other" other) i) ++ suffix
+nestedTuple : Expression -> Int -> String
+nestedTuple a i  =
+    case flattenApplication a of
+        [Application left right] ->
+            elixirE left i ++ ".(" ++ elixirE right i ++ ")"
 
+        Variable [name] :: rest ->
+            "{:"
+            ++ String.toLower name
+            ++ ", "
+            ++ (map (\a -> elixirE a i) rest |> String.join ", ")
+            ++ "}"
+
+        other -> Debug.crash ("Shouldn't ever work for" ++ toString other)
+
+flattenApplication : Expression -> List Expression
+flattenApplication application =
+    case application of
+        Application left right ->
+            if isTuple application then
+                (flattenApplication left) ++ [right]
+            else
+                [application]
+        other -> [other]
 
 nothing : String
 nothing = "\"No value in Maybe\""
