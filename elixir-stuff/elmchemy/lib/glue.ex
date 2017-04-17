380b14904c87eb923830c75744ebb35bbc961cb5
Krzysztof Wende <chris@neontree.pl>
2017-04-17 16:14:23 +0200

    nicer curry

--- elixir-stuff/elmchemy/lib/glue.ex
+++ elixir-stuff/elmchemy/lib/glue.ex
@@ -1,34 +1,60 @@
 defmodule Elmchemy.Glue do
 
-  defmacro defcurryp(definition, opts \\ [], do: body) do
-    {fun, args} = Macro.decompose_call(definition)
-    quote do
-      defp unquote(fun)() do
-        unquote(curry(args, body))
-      end
-      defp unquote(fun)(unquote_splicing(args)) do
-        unquote(body)
-      end
-    end
+  defmacro curry({:/, _, [{name, _, _}, arity]}) do
+    args =
+      1..arity |> Enum.map(&({:"arg #{&1}", [], Elixir}))
 
+    define_curried(:def, name, args)
   end
 
-  defmacro defcurry(definition, opts \\ [], do: body) do
+  defmacro curryp({:/, _, [{name, _, _}, arity]}) do
+    args =
+      1..arity |> Enum.map(&({:"arg #{&1}", [], Elixir}))
+
+    define_curried(:defp, name, args)
+  end
+
+  defmacro defcurry(definition, _opts \\ [], do: body) do
     {fun, args} = Macro.decompose_call(definition)
     quote do
-      def unquote(fun)() do
-        unquote(curry(args, body))
-      end
+      unquote(define_curried(:def, fun, args))
       def unquote(fun)(unquote_splicing(args)) do
         unquote(body)
       end
     end
   end
 
-  defp curry([h | args], body) do
-    quote do fn unquote(h) -> unquote(curry(args, body)) end end
+  defmacro defcurryp(definition, _opts \\ [], do: body) do
+    {fun, args} = Macro.decompose_call(definition)
+    quote do
+      unquote(define_curried(:defp, fun, args))
+      defp unquote(fun)(unquote_splicing(args)) do
+        unquote(body)
+      end
+    end
   end
-  defp curry([], body), do: body
 
+  defp define_curried(:def, name, args) do
+    quote do
+      def unquote(name)() do
+        unquote(do_curry(name, args))
+      end
+    end
+  end
+  defp define_curried(:defp, name, args) do
+    quote do
+      defp unquote(name)() do
+        unquote(do_curry(name, args))
+      end
+    end
+  end
+
+  defp do_curry(fun, args), do: do_curry(fun, args, args)
+  defp do_curry(fun, [h | args], all) do
+    quote do fn unquote(h) -> unquote(do_curry(fun, args, all)) end end
+  end
+  defp do_curry(fun, [], args) do
+    quote do: unquote(fun)(unquote_splicing(args))
+  end
 
 end
--- src/Compiler.elm
+++ src/Compiler.elm
@@ -275,15 +275,19 @@ elixirS s c =
             if name == "meta" && args == [] then
                 generateMeta body
             else if List.length args > 1 then
-                genElixirFunc c name args body
+                (ind c.indent)
+                ++ "curry "
+                ++ name ++ "/" ++ toString (List.length args)
+                ++ genElixirFunc c name args body
                     ++ "\n"
             else
                 case body of
                     Case _ expressions ->
-                        expressions
+                        (ind c.indent) ++ "curry " ++ name ++ "/1"
+                        ++ (expressions
                             |> List.map (\( left, right ) -> genElixirFunc c name [ elixirE left c.indent ] right)
                             |> List.foldr (++) ""
-                            |> flip (++) "\n"
+                            |> flip (++) "\n")
 
                     _ ->
                         genElixirFunc c name args body
@@ -308,9 +312,9 @@ defOrDefp context name =
     case context.exports of
         SubsetExport exports ->
             if any (\exp -> exp == FunctionExport name) exports then
-                "defcurry "
+                "def "
             else
-                "defcurryp "
+                "defp "
 
         AllExport ->
             "def "
