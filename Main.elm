505b06480ba645dd61fa1927ef61fb283d6ae4cd
Tomasz Cichocinski <tomaszcichocinski@gmail.com>
2017-04-20 16:29:24 +0200

    CodeMirror for code editing

--- Main.elm
+++ Main.elm
@@ -1,4 +1,4 @@
-module Main exposing (main)
+port module Main exposing (main)
 
 import Html.Events exposing (..)
 import Json.Decode as JD
@@ -14,124 +14,39 @@ type Msg
     | String
 
 
-divStyle : Attribute msg
-divStyle =
-    Html.Attributes.style
-        [ ( "display", "inline-flex" )
-        , ( "width", "100%" )
-        ]
-
-
-codeStyle : Attribute msg
-codeStyle =
-    Html.Attributes.style
-        [ ( "width", "50%" )
-        , ( "margin", "10px" )
-        , ( "font-family", "monospace" )
-        , ( "font-size", "13px" )
-        ]
-
-
 view : String -> Html Msg
 view model =
-    div [ divStyle ]
-        [ textarea [ codeStyle, on "input" (JD.map Replace targetValue) ] [ text model ]
-        , Markdown.toHtml [] <| "```elixir\n" ++ (Compiler.tree model) ++ "\n```"
-        ]
+    Markdown.toHtml [] <| "```elixir\n" ++ (Compiler.tree model) ++ "\n```"
 
 
-main : Program Never String Msg
+init : String -> ( String, Cmd Msg )
+init v =
+    ( v, Cmd.none )
+
+
+main : Program String String Msg
 main =
-    Html.beginnerProgram
-        { model = init
+    Html.programWithFlags
+        { init = init
         , update = update
         , view = view
+        , subscriptions =
+            (\_ ->
+                Sub.batch
+                    [ updateInput Replace
+                    ]
+            )
         }
 
 
-init : String
-init =
-    """module Stack exposing (..)
-
-import Elmchemy exposing (..)
-
-
-meta =
-    [ "use GenServer" ]
-
-
-type alias State a =
-    List a
-
-
-type GenServerReturn a b
-    = Reply a (State b)
-    | NoReply (State b)
-
-
-type Command a
-    = Stack
-    | Push a
-    | Pop
-
-
-
--- Client
-
-
-startLink : a -> Pid
-startLink default =
-    ffi "GenServer" "start_link" ( Stack, default )
-
-
-push : Pid -> a -> a
-push pid item =
-    ffi "GenServer" "cast" ( pid, (Push item) )
-
-
-pop : Pid -> a
-pop pid =
-    ffi "GenServer" "call" ( pid, Pop )
-
-
-
--- Server (callbacks)
-
-
-handle_call : Command a -> Pid -> State a -> GenServerReturn a a
-handle_call command from state =
-    case ( command, from, state ) of
-        Pop, _, (h :: t) ->
-            Reply h t
-
-        ( request, from, state ) ->
-            lffi "super" ( request, from, state )
-
-
-handle_cast : Command a -> State a -> GenServerReturn a a
-handle_cast command state =
-    case ( command, state ) of
-        (Push item), state ->
-            NoReply (item :: state)
-
-        ( request, state ) ->
-            lffi "super" ( request, state )
-"""
-
-
-type Czlowiek a
-    = List a
-
-
-type Wende
-    = Czlowiek Int
-
-
-update : Msg -> String -> String
+update : Msg -> String -> ( String, Cmd Msg )
 update action model =
     case action of
         Replace m ->
-            m
+            ( m, Cmd.none )
 
         String ->
-            ""
+            ( "", Cmd.none )
+
+
+port updateInput : (String -> msg) -> Sub msg
