98642c95e37568db445cb27db84ed337c24d28bc
Tomasz Cichocinski <tomaszcichocinski@gmail.com>
2017-04-29 22:56:18 +0200

    Rewrite toSnakeCase helper function

--- src/Helpers.elm
+++ src/Helpers.elm
@@ -3,6 +3,7 @@ module Helpers exposing (..)
 import Char
 import Tuple exposing (..)
 import List exposing (..)
+import Regex
 
 
 type MaybeUpper
@@ -22,32 +23,18 @@ notImplemented feature value =
 
 
 toSnakeCase : String -> String
-toSnakeCase s =
+toSnakeCase var =
     let
-        res =
-            toSnakeCase_ s
+        split =
+            Regex.regex "(?=[A-Z])"
     in
-        if String.startsWith "_" res && res /= "_" then
-            res |> String.dropLeft 1
+        if String.toUpper var == var then
+            String.toLower var
         else
-            res
-
-
-toSnakeCase_ : String -> String
-toSnakeCase_ e =
-    let
-        withUpper =
-            \a -> ( first a |> Char.isUpper, first a, second a )
-    in
-        case String.uncons e |> Maybe.map withUpper of
-            Just ( True, a, rest ) ->
-                "_" ++ String.cons (Char.toLower a) (toSnakeCase_ rest)
-
-            Just ( False, a, rest ) ->
-                String.cons a (toSnakeCase_ rest)
-
-            Nothing ->
-                ""
+            var
+                |> Regex.split Regex.All split
+                |> String.join "_"
+                |> String.toLower
 
 
 isUpper : String -> Bool
