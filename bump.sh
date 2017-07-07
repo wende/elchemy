6542cdf99cd67d5e4775d3f00eb83d6a18e3de31
Krzysztof Wende <chris@neontree.pl>
2017-07-07 19:17:44 +0200

    ./bump releases both versions

--- bump.sh
+++ bump.sh
@@ -13,6 +13,10 @@ if git diff-index --quiet HEAD --; then
     git commit -am "Release $VER"
     git tag $VER
     git push origin master $VER
+
+    sed -i "" "s/name\": \"elchemy\"/name\": \"elmchemy\"/g" package.json
+    npm publish
+    sed -i "" "s/name\": \"elmchemy\"/name\": \"elchemy\"/g" package.json
     npm publish
 else
     echo "Git directory must be clean"
