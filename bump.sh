f5c523ae14a2e2276945bf800c67efd37d80b870
Krzysztof Wende <chris@neontree.pl>
2017-07-15 22:08:48 +0200

    XDict

--- bump.sh
+++ bump.sh
@@ -3,17 +3,36 @@
     exit 0
 fi
 if git diff-index --quiet HEAD --; then
-    git pull origin master
     npm version $1
     SEMVER='[0-9][0-9]*\.[0-9][0-9]*\.[0-9]*'
     VER=`npm ls | grep -o $SEMVER`
-    sed -i "" "s/$SEMVER/$VER/g" src/Compiler.elm
-    make compile
-    make release
+
+    make compile-std
+    cd elchemy-core
+    sed -i "" "s/$SEMVER/$VER/g" mix.exs
+    git pull origin master
     git commit -am "Release $VER"
     git tag $VER
     git push origin master $VER
+    rm -f elchemy-*.ez
+    mix archive.build
+    mix archive.install "elchemy-$VER.ez" --force
+
+    cd ..
+    git pull origin master
+    sed -i "" "s/$SEMVER/$VER/g" src/Compiler.elm
+    make compile
+    make release
+
+    sed -i "" "s/name\": \"elchemy\"/name\": \"elmchemy\"/g" package.json
     npm publish
+    sed -i "" "s/name\": \"elmchemy\"/name\": \"elchemy\"/g" package.json
+    sed -i "" "s/$SEMVER/$VER/g" ./elchemy
+    npm publish
+
+    git commit -am "Release $VER"
+    git tag $VER
+    git push origin master $VER
 else
     echo "Git directory must be clean"
     exit 1
