be94a0e3bba1465546b766ece7f8392f7793ae82
Haito <hajtosek@gmail.com>
2017-07-28 23:22:21 +0200

    Now generating missing files

--- installer/lib/elchemy_new/new_project.ex
+++ installer/lib/elchemy_new/new_project.ex
@@ -5,7 +5,10 @@ defmodule Elchemy.NewProject do
     {:eex, "new/mix.exs", "mix.exs"},
     {:eex, "new/README.md", "README.md"},
     {:eex, "new/config/config.exs", "config/config.exs"},
-    {:eex, "new/elm/AppName.elm", "elm/:app_module.elm"}
+    {:eex, "new/elm/AppName.elm", "elm/:app_module.elm"},
+    {:eex, "new/elm-package.json", "elm-package.json"},
+    {:eex, "new/test/test_helper.exs", "test/test_helper.exs"},
+    {:eex, "new/test/test_template.exs", "test/:app_name.exs"}
   ]
 
 end
