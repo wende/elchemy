acdddb70d101b2b108cd2b45cce1dd737142486b
Krzysztof Wende <krzysztof.wende@gmail.com>
2017-10-29 16:29:27 +0100

    release pipeline changed and -n option added to bump

--- bump.sh
+++ bump.sh
@@ -3,9 +3,12 @@
     exit 0
 fi
 if git diff-index --quiet HEAD --; then
+    CHANGELOG=`git changelog -x --tag VER`
     npm version $1
     SEMVER='[0-9][0-9]*\.[0-9][0-9]*\.[0-9]*'
     VER=`npm ls | grep -o elchemy@$SEMVER | grep -o $SEMVER`
+    CHANGELOG=${CHANGELOG/VER/$VER}
+    echo "$CHANGELOG"
 
     make compile-std
     cd elchemy-core
@@ -17,7 +20,7 @@ if git diff-index --quiet HEAD --; then
     if ! [[ $* == *-n* ]]; then
       git push origin master $VER
     fi
-    
+
     cd ..
     sed -i "" "s/$SEMVER/$VER/g" mix.exs
     rm -f elchemy-*.ez
@@ -38,8 +41,6 @@ if git diff-index --quiet HEAD --; then
     if ! [[ $* == *-n* ]]; then
       npm publish
     fi
-
-    CHANGELOG=`git changelog -x --tag $VER`
 
     git commit -am "$CHANGELOG"
     git tag $VER
