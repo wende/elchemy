3978500d8bd41052ca5fb1a3cc991a6572b10174
Jakub Hajto <hajtosek@gmail.com>
2017-11-26 14:50:03 +0100

    Modified templates

--- installer/templates/new/test/test_template.exs
+++ installer/templates/new/test/test_template.exs
@@ -1,5 +1,11 @@
-defmodule CharacterTest do
-  use ExUnit.Case
-  use Elmchemy
+defmodule <%= app_module %>Test do
+   use ExUnit.Case
+   use Elchemy
 
+   doctest <%= app_module %>
+   typetest <%= app_module %>
+
+    test do
+      assert <%= app_module %>.hello == "world"
+    end
 end
