564ceb6c5919d00818254fef682a72d9e3e4fdba
colinbankier <colinbankier@gmail.com>
2017-12-04 15:28:46 +1000

    Add elchemy path to project init.

--- elchemy.exs
+++ elchemy.exs
@@ -2,6 +2,7 @@ defmodule ElchemyInit do
   def init(project, {__MODULE, _}) do
     project
     |> put_in([:compilers], [:elchemy | (project[:compilers] || [])])
+    |> put_in([:elchemy_path], "elm")
     |> put_in([:deps], project[:deps] ++ deps())
   end
 
