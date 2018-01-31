da11e1104b1f05702f20c31515167558efd57b93
Krzysztof Wende <krzysztof.wende@gmail.com>
2018-01-31 02:15:22 +0100

    now fail mfck

--- src/ExStatement.elm
+++ src/ExStatement.elm
@@ -224,6 +224,12 @@ elixirS c s =
                                 (Application (Application (Variable [ "tryFfi" ]) _) _) as app ->
                                     genFfi app
 
+                                Case ((Variable [ name ]) as var) expressions ->
+                                    if [ var ] == args then
+                                        ExFunction.genOverloadedFunctionDefinition c ExExpression.elixirE name args body expressions
+                                    else
+                                        ExFunction.genFunctionDefinition c ExExpression.elixirE name args body
+
                                 Case (Tuple vars) expressions ->
                                     if vars == args && List.all (Tuple.first >> isTuple) expressions then
                                         ExFunction.genOverloadedFunctionDefinition c ExExpression.elixirE name args body expressions
