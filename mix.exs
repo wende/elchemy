3f28abf5533f28e7f4ce9f668e3cc93996865c38
Krzysztof Wende <krzysztof.wende@gmail.com>
2018-05-23 14:08:57 +0200

    0.7.3 / 2018-05-23 ================
      * Clean makes init not able to work (#341)
      * No project name (#339)
      * Correct typos in roadmap/STRUCTURES.md (#319)
      * Added formating and test (#337)
      * 0.7.2 / 2018-05-16 ================
      * 0.7.2
      * Closes #276 macro plugins (#331)
      * Update elm-package.json
      * Bug: Closes #327 (#328)
      * Closes #329 - Warn when elchemy not installed during project compilation (#330)
      * Closes #238 - Debug.log print in Elchemy format instead of Elixir format (#325)
      * Closes #322 Warn when elchemy line put incorrectly (#323)
      * Update README.md (#320)
      * correct typos (#318)
      * type savety and Elixir (#310)
      * Closes #313 Fixes interop in gitbook and new gitbbok (#317)
      * Corrected typo, set Compiler to 90% (#314)
      * Corrected typo (#306)
      * Corrected typos (#307)
      * correct typo (#308)
      * Delete FEATURES.MD
      * Create CODE_OF_CONDUCT.md (#304)
      * Typo in gitbook (#305)
      * 0.6.6 / 2018-03-05 ================
      * 0.6.6
      * format properly
      * 0.6.5 / 2018-03-05 ================
      * 0.6.5
      * format when > 1.6.0 (#300)
      * Update README.md
      * Better references in maturity of the project summary
      * Update SIDE_EFFECTS.md
      * Update SUMMARY.md
      * Update SUMMARY.md
      * Add SYNTAX OVERVIEW
      * Create SYNTAX.md
      * Fix to 292 (#293)
      * Closes #290 - incremental compiler bug  (#291)
      * 0.6.4 / 2018-02-07 ================
      * 0.6.4
      * Updated core

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Elchemy.Mixfile do
     [app: :elchemy,
      name: "Elchemy Compiler",
      description: "Mix compiler wrapper around Elchemy project",
-     version: "0.7.2",
+     version: "0.7.3",
      elixir: "~> 1.4",
      description: "",
      package: package(),
