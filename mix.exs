2f4aca0dbfe2504fef4fbc057988cb6914ccda40
Krzysztof Wende <iraasta@gmail.com>
2018-09-22 20:40:52 +0200

    0.8.0 / 2018-09-22 ================

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Elchemy.Mixfile do
     [app: :elchemy,
      name: "Elchemy Compiler",
      description: "Mix compiler wrapper around Elchemy project",
-     version: "0.7.4",
+     version: "0.8.0",
      elixir: "~> 1.4",
      description: "",
      package: package(),
