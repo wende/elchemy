47ff02a2ac95c0def2737c1d8246d6263b4420a6
Kacper Latuszewski <kacpx123@gmail.com>
2018-09-27 19:20:17 +0200

    Changed if statement to smart regex and used sysytem version

--- lib/mix/tasks/compile.elchemy.ex
+++ lib/mix/tasks/compile.elchemy.ex
@@ -32,6 +32,6 @@ defmodule Mix.Tasks.Compile.Elchemy do
     Mix.Task.run "deps.compile"
     IO.puts "-- Elchemy compilation complete --\n"
 
-    if Regex.match?(~r/1.[7-9].[0-100]/, version), do: IO.warn "Elixir 1.7 version doesn't work with Elchemy. You have to use lower."
+    if Regex.match?(~r/1.[7-9].[0-9]+$/, version), do: IO.warn "Elixir 1.7 version doesn't work with Elchemy. You have to use lower."
   end
 end
